b1d7a10eb30877dfa63773f4507d77a0f835dd87
Mihail <scrayerful@gmail.com>
2020-02-26 00:40:48 +0500

    Add render test

--- PlusPlusGame/win32_platform.cpp
+++ PlusPlusGame/win32_platform.cpp
@@ -1,8 +1,19 @@
 #include <Windows.h>
 
-bool running(false);
+bool running(true);
 
-LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+struct RenderState
+{
+	int height, width;
+	void * memory;
+
+	BITMAPINFO bitmapInfo;
+};
+
+RenderState renderState;
+
+
+LRESULT CALLBACK windowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	LRESULT result(0);
 	switch (uMsg)
@@ -13,24 +24,45 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 	{
 		running = false;
 	}
+	case WM_SIZE:
+	{
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		renderState.width = rect.right - rect.left;
+		renderState.height = rect.bottom - rect.top;
+
+		int bufferSize = renderState.width * renderState.height * sizeof(unsigned int);
+
+		if (renderState.memory) VirtualFree(renderState.memory, 0, MEM_RELEASE);
+
+		renderState.memory = VirtualAlloc(0, bufferSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+		renderState.bitmapInfo.bmiHeader.biSize = sizeof(renderState.bitmapInfo.bmiHeader);
+		renderState.bitmapInfo.bmiHeader.biWidth = renderState.width;
+		renderState.bitmapInfo.bmiHeader.biHeight = renderState.height;
+		renderState.bitmapInfo.bmiHeader.biPlanes = 1;
+		renderState.bitmapInfo.bmiHeader.biBitCount = 32;
+		renderState.bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+	}
 	break;
-	default: result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+	default:
+		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return result;
 }
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-	WNDCLASS window_class = {};
-	window_class.style = CS_HREDRAW | CS_VREDRAW;
-	window_class.lpszClassName = "Game Window Class";
-	window_class.lpfnWndProc = window_callback;
+	WNDCLASS windowClass = {};
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpszClassName = "Game Window Class";
+	windowClass.lpfnWndProc = windowCallback;
 
-	RegisterClass(&window_class);
+	RegisterClass(&windowClass);
 
-	HWND window = CreateWindow(window_class.lpszClassName, "PlusPlusGame", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
-
-	running = true;
+	HWND window = CreateWindow(windowClass.lpszClassName, "PlusPlusGame", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
 
 	while (running)
 	{
@@ -40,5 +72,17 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 			TranslateMessage(&message);
 			DispatchMessage(&message);
 		}
+
+		unsigned int * pixel = (unsigned int*)renderState.memory;
+		for (int y = 0; y < renderState.height; y++)
+		{
+			for (int x = 0; x < renderState.width; x++)
+			{
+				*pixel++ = 0xff2277*rand();
+			}
+		}
+
+		StretchDIBits(hdc, 0, 0, renderState.width, renderState.height, 0, 0, renderState.width, renderState.height, renderState.memory, &renderState.bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+
 	}
 }
